abf6ebc352a9f64f7890149f08daaafea0f59804
ToruNAKANDAKARI <e125726@ie.u-ryukyu.ac.jp>
2015-12-13 15:25:49 +0900

    スタート,ステージ画面のViewControllerファイルを追加

---
+++ ColoplGame/ColoplGame/StageSelectViewController.swift
@@ -0,0 +1,20 @@
+//
+//  StageSelectViewController.swift
+//  ColoplGame
+//
+//  Created by ToruNakandakari on H27/12/13.
+//  Copyright © 平成27年 ToruNakandakari. All rights reserved.
+//
+
+import UIKit
+
+class StageSelectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
