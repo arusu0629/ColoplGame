a01f23936b5b188d1bba43d6bba696274fb61efb
ToruNAKANDAKARI <e125726@ie.u-ryukyu.ac.jp>
2016-01-01 21:22:13 +0900

    各SNSにシェアするためのヘルパークラスを作成した

--- ColoplGame/ColoplGame/SNSHelper.swift
+++ ColoplGame/ColoplGame/SNSHelper.swift
@@ -7,7 +7,35 @@
 //
 
 import UIKit
+import Social
 
 class SNSHelper: NSObject {
+    class func postToTwitter(currentViewController currentVC: UIViewController) {
+        let composeVC = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+        let clearCount = RealmHelper.getAllClearData().count
+        let postText = "現在\(clearCount)つのステージをクリアしています #ColoplGame"
+        composeVC.setInitialText(postText)
+        currentVC.presentViewController(composeVC, animated: true, completion: nil)
+    }
+    
+    class func postToFaceBook(currentViewController currentVC: UIViewController) {
+        let composeVC = SLComposeViewController(forServiceType: SLServiceTypeFacebook)
+        let clearCount = RealmHelper.getAllClearData().count
+        let postText = "現在\(clearCount)つのステージをクリアしています #ColoplGame"
+        composeVC.setInitialText(postText)
+        currentVC.presentViewController(composeVC, animated: true, completion: nil)
+    }
+
+    class func postToLine(currentViewController currentVC: UIViewController) {
+        let clearCount = RealmHelper.getAllClearData().count
+        let postText = "現在\(clearCount)つのステージをクリアしています #ColoplGame"
+        let encodeMessage = postText.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+        let messageURL: NSURL! = NSURL(string: "line://msg/text/" + encodeMessage!)
+        if (UIApplication.sharedApplication().canOpenURL(messageURL)) {
+            UIApplication.sharedApplication().openURL( messageURL )
+        } else {
+            print("無効なURLです")
+        }
+    }
 
 }
